e4ea3716ae34dc5d8f27f28d632fa539bf06c490
Mitch Tang <m3tang@gmail.com>
2025-04-23 14:02:11 -0700

    add & edit forms

--- app/query/route.ts
+++ app/query/route.ts
@@ -3,16 +3,18 @@ import postgres from "postgres";
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 async function listInvoices() {
-  const data = await sql`
+  try {
+    const data = await sql`
     SELECT invoices.amount, customers.name
     FROM invoices
     JOIN customers ON invoices.customer_id = customers.id
     WHERE invoices.amount = 666;
   `;
 
-  console.log(data);
-
-  return data;
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export async function GET() {
--- app/seed/route.ts
+++ app/seed/route.ts
@@ -103,11 +103,11 @@ async function seedRevenue() {
 
 export async function GET() {
   try {
-    const result = await sql.begin(async (sql) => [
-      await seedUsers(),
-      await seedCustomers(),
-      await seedInvoices(),
-      await seedRevenue(),
+    await sql.begin(() => [
+      seedUsers(),
+      seedCustomers(),
+      seedInvoices(),
+      seedRevenue(),
     ]);
 
     return Response.json({ message: "Database seeded successfully" });
